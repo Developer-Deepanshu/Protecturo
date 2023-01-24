7f7647c0b1b271d2ccf539356249820aafe9b8d4
gunjan <gunjandora595@gmail.com>
2023-01-25 00:42:34 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import "./App.css";
 import "./assets/css/root.css";
-
+import "./assets/css/style.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
 import Ourproducts from "./components/Ourproducts";
+import Benefits from "./components/Benefits";
+import Works from "./components/Works";
 
 function App() {
   return (
     <>
       <Ourproducts />
+      <Benefits />
+      <Works />
     </>
   );
 }
