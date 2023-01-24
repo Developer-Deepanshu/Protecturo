a19323a9031331a149c7296a6d761572be57e24e
developer-deepanshu <developerdeepanshu07@gmail.com>
2023-01-24 16:45:49 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import './App.css';
 import './assets/css/root.css';
 import Hero from './components/Hero';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div>
-      <Hero />
+      <NavBar />
     </div>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function NavBar() {
+    const [open, setclose] = useState(false);
+
     return (
         <>
             <nav>
