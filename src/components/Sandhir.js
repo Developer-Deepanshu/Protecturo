24f38ab908553a8097e7323d1860acf6bc716ee0
sandhirradialcode <sandhirradialcode.com>
2023-01-25 09:31:29 +0530

    ok

--- src/components/Sandhir.js
+++ src/components/Sandhir.js
@@ -1,30 +1,7 @@
-import "./App.css";
-import "./assets/css/root.css";
-import "./assets/css/style.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle";
-import Ourproducts from "./components/Ourproducts";
-import Benefits from "./components/Benefits";
-import Works from "./components/Works";
-import NavBar from "./components/NavBar";
-import Hero from "./components/Hero";
-import TakeControl from "./components/TakeControl";
+import React from "react";
 
-function App() {
-    return (
-        <>
-            <div className="min-vh-100 position-relative overflow-hidden">
-                <NavBar />
-                <Hero />
-            </div>
-            <TakeControl />
-            <Ourproducts />
-            <Benefits />
-            <Works />
-        </>
-
-    )
+function Sandhir() {
+  return <div>Sandhir</div>;
 }
-export default App
 
-export default Sandhir
+export default Sandhir;
