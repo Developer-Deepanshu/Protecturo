998b7de614866e1615967257d76d6f4267c6311b
developer-deepanshu <developerdeepanshu07@gmail.com>
2023-01-25 09:29:50 +0530

    ok

--- src/components/Sandhir.js
+++ src/components/Sandhir.js
@@ -1,28 +1,9 @@
-import "./App.css";
-import "./assets/css/root.css";
-import "./assets/css/style.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle";
-import Ourproducts from "./components/Ourproducts";
-import Benefits from "./components/Benefits";
-import Works from "./components/Works";
-import NavBar from "./components/NavBar";
-import Hero from "./components/Hero";
-import TakeControl from "./components/TakeControl";
+import React from 'react'
 
-function App() {
+function Sandhir() {
     return (
-        <>
-            <div className="min-vh-100 position-relative overflow-hidden">
-                <NavBar />
-                <Hero />
-            </div>
-            <TakeControl />
-            <Ourproducts />
-            <Benefits />
-            <Works />
-        </>
-
+        <div>Sandhir</div>
     )
 }
-export default App
+
+export default Sandhir
