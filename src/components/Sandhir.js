f6cf37d3501e9708a083c7394236238e7e0241ef
sandhirradialcode <sandhirradialcode.com>
2023-01-25 09:28:23 +0530

    ok

--- src/components/Sandhir.js
+++ src/components/Sandhir.js
@@ -9,20 +9,21 @@ import Works from "./components/Works";
 import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
 import TakeControl from "./components/TakeControl";
+import Features from "";
 
 function App() {
-    return (
-        <>
-            <div className="min-vh-100 position-relative overflow-hidden">
-                <NavBar />
-                <Hero />
-            </div>
-            <TakeControl />
-            <Ourproducts />
-            <Benefits />
-            <Works />
-        </>
-
-    )
+  return (
+    <>
+      <div className="min-vh-100 position-relative overflow-hidden">
+        <NavBar />
+        <Hero />
+      </div>
+      <TakeControl />
+      <Ourproducts />
+      <Benefits />
+      <Works />
+      <Features />
+    </>
+  );
 }
-export default App
+export default App;
