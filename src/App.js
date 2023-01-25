858b503f19fa78e0d2f807575555216f58a8b03b
developer-deepanshu <developerdeepanshu07@gmail.com>
2023-01-25 09:01:48 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import "./assets/css/root.css";
 import Hero from "./components/Hero";
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavBar from "./components/NavBar";
+import TakeControl from "./components/TakeControl";
 
 function App() {
   return (
-    <div>
-      <NavBar />
-    </div>
+    <>
+      <div className="min-vh-100 position-relative overflow-hidden">
+        <NavBar />
+        <Hero />
+      </div>
+      <TakeControl />
+    </>
   );
 }
 
