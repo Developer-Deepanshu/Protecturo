a8ebaf1410b8a153a01d923d33d3ede2e6d57050
developer-deepanshu <developerdeepanshu07@gmail.com>
2023-01-25 12:24:18 +0530

    ok

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,10 +7,10 @@ function Hero() {
             <div className='bg_hero flex-grow-1 position-relative'>
                 <img className='w-100 position-absolute bottom-0' src={hero_shadow} alt="hero_shadow" />
                 <div className='container'>
-                    <div className='pb_250_max_xl'>
+                    <div className='pb_250_max_xl pt-5'>
                         <div className='hero_line m-auto'></div>
-                        <h1 className='fw-bold ff_bold_gilroy fs_xl_3 text-center text_white_3
-                     mw_460 m-auto'>Cyber safety starts
+                        <h1 className='fw-bold ff_bold_gilroy xl_3 text-center text_white_3
+                     mw_980 m-auto'>Cyber safety starts
                             here. For both commercial
                             and personal.</h1>
                     </div>
