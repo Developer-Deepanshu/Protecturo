ff97270fba23ca356a1516f7de6f7695d526fd57
sandhirradialcode <sandhirradialcode.com>
2023-01-25 09:14:56 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import "./App.css";
 import "./assets/css/root.css";
-import Hero from "./components/Hero";
+import "./assets/css/style.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
 import Ourproducts from "./components/Ourproducts";
 import Benefits from "./components/Benefits";
 import Works from "./components/Works";
 import NavBar from "./components/NavBar";
+import Hero from "./components/Hero";
+import TakeControl from "./components/TakeControl";
 
 function App() {
   return (
-    <div>
-      <NavBar />
-    </div>
+    <>
+      <div className="min-vh-100 position-relative overflow-hidden">
+        <NavBar />
+        <Hero />
+      </div>
+      <TakeControl />
+      <Ourproducts />
+      <Benefits />
+      <Works />
+    </>
   );
 }
 export default App;
