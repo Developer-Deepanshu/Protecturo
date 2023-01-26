cb3f12284618dde86fb8c0c16bb9f5573bec6243
developer-deepanshu <developerdeepanshu07@gmail.com>
2023-01-26 11:56:55 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
 import "./assets/css/root.css";
-import "./assets/css/style.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
 import Ourproducts from "./components/Ourproducts";
--- src/components/Loding.jsx
+++ src/components/Loding.jsx
@@ -5,8 +5,8 @@ function Loding() {
     return (
         <div>
             <div className='min-vh-100 bg-black loding_position'>
-                <div className='animation_loding'>
-                    <img className='w-100' src={logo_loading} alt="loading" />
+                <div className=''>
+                    <img className='w-100 animation_loding' src={logo_loading} alt="loading" />
                 </div>
             </div>
         </div>
